7f123fef270116346405b49ba72b3137833518ba
AmeeshaSaxena0398 <71024355+AmeeshaSaxena0398@users.noreply.github.com>
2021-03-10 18:43:49 +0530

    Add files via upload

---
+++ JarExecutable/src/com/hcl/runner/Runner.java
@@ -0,0 +1,13 @@
+package com.hcl.runner;
+
+import com.hcl.jar.Calculator;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		System.out.println(cal.add(4, 10));
+
+	}
+
+}
